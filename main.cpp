8b80d4bde1e510614511c151eee2459a7c3298ca
Alexandr Suromkin <xxgeniuszz@mail.ru>
2023-12-07 00:16:00 +0600

    Second commit

--- main.cpp
+++ main.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <utility>
 #include <vector>
 #include <thread>
 #include <mutex>
-#include <atomic>
-#include <condition_variable>
-#include <sstream>
 
-bool compare_results(const std::string& a, const std::string& b) {
-    std::istringstream issA(a);
-    std::istringstream issB(b);
-    int lineA, lineB;
-    issA >> lineA;
-    issB >> lineB;
-    return lineA < lineB;
+struct result{
+    result(size_t line_num, size_t line_index, std::string line)
+        : line_num(line_num), line_index(line_index), line(std::move(line)){}
+    size_t line_num;
+    size_t line_index;
+    std::string line;
+    void show_result() const
+    {
+        std::cout<<line_num<<" "<<line_index<<" "<<line<<std::endl;
+    }
+};
+
+bool compare_results(const result& a, const result& b) {
+    return a.line_num < b.line_num;
 }
 
 bool match_mask(const std::string &str, int start, const std::string &mask) {
@@ -26,34 +30,40 @@ bool match_mask(const std::string &str, int start, const std::string &mask) {
     return true;
 }
 
-int count_lines(const std::string& filename) {
+int count_lines(const std::string& filename, std::vector<std::string>& fileStr) {
     std::ifstream file(filename);
     int count = 0;
     std::string line;
 
+    if (!file) {
+        std::cerr << "Не удалось открыть файл: " << filename << std::endl;
+        return -1;
+    }
+
     while (std::getline(file, line)) {
+        fileStr.push_back(line);
         ++count;
     }
 
     return count;
 }
 
-void find_in_segment(const std::string& filename, int start_line, int end_line,
-                     const std::string& mask, std::vector<std::string>& results,
+void find_in_segment(const std::vector<std::string>& fileStr, size_t start_line, size_t end_line,
+                     const std::string& mask, std::vector<result>& results,
                      std::mutex& results_mutex) {
-    std::ifstream file(filename);
-    std::string line;
-    int line_number = 0;
 
-    while (getline(file, line)) {
-        line_number++;
-        if (line_number < start_line || line_number > end_line) continue;
+    for(size_t line_number = start_line; line_number < end_line; line_number++)  {
+        if(line_number >= fileStr.size()){
+            break;
+        }
+        const std::string& line = fileStr[line_number];
 
         for (size_t i = 0; i <= line.length() - mask.length(); ++i) {
             if (match_mask(line, i, mask)) {
                 std::lock_guard<std::mutex> lock(results_mutex);
-                results.push_back(std::to_string(line_number) + " " + std::to_string(i + 1) + " " + line.substr(i, mask.length()));
-                break; // Для предотвращения пересечений вхождений
+                result res(line_number, i + 1, line.substr(i, mask.length()));
+                results.push_back(res);
+                break;
             }
         }
     }
@@ -67,21 +77,21 @@ int main(int argc, char* argv[]) {
 
     std::string filename = argv[1];
     std::string mask = argv[2];
-    std::vector<std::string> results;
+    std::vector<result> results;
     std::mutex results_mutex;
 
-    // Пример: 4 потока
     size_t num_threads = std::thread::hardware_concurrency();
     std::vector<std::thread> threads;
-    size_t all_lines = count_lines(filename);
+    std::vector<std::string> file;
+    size_t all_lines = count_lines(filename, std::ref(file));
     if(all_lines < num_threads){
         num_threads = all_lines;
     }
     size_t lines_per_thread = std::ceil(all_lines / double (num_threads));
 
     for (int i = 0; i < num_threads; ++i) {
-        threads.emplace_back(find_in_segment, filename,
-                             i * lines_per_thread + 1, (i + 1) * lines_per_thread,
+        threads.emplace_back(find_in_segment, file,
+                             i * lines_per_thread, (i + 1) * lines_per_thread,
                              mask, std::ref(results), std::ref(results_mutex));
     }
 
@@ -92,8 +102,8 @@ int main(int argc, char* argv[]) {
     std::sort(results.begin(), results.end(), compare_results);
 
     std::cout << results.size() << std::endl;
-    for (const auto& match : results) {
-        std::cout << match << std::endl;
+    for (const auto& res : results) {
+        res.show_result();
     }
 
     return 0;
